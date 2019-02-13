03e6fb066e3c834746708c5bdc9448007fd85232
Jeong Yong Su <knarrblue@gmail.com>
2019-02-13 22:37:04 +0900

    add dp

---
+++ programmers/Level 2/피보나치 수/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int dp[100001];
+int solution(int n) {
+    dp[0]=0;
+    dp[1]=1;
+    
+    for(int i=2; i<=n; ++i){
+        dp[i] = dp[i-1]%1234567 + dp[i-2]%1234567;
+    }
+
+    return dp[n]%1234567;
+}
