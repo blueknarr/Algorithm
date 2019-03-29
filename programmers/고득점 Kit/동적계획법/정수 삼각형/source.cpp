1fb1c322b1cef8031be2a1b7adae3f3c67b3b973
Jeong Yong Su <knarrblue@gmail.com>
2019-03-29 17:15:04 +0900

    누적 최대값 찾기

--- programmers/고득점 Kit/동적계획법/정수 삼각형/source.cpp
+++ programmers/고득점 Kit/동적계획법/정수 삼각형/source.cpp
@@ -1,17 +1,23 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int dp[500][500];
+
 int solution(vector<vector<int>> triangle) {
-    int answer = triangle[0][0];
-    dp[0][0] = answer;
-    
-    for(int i=1; i<triangle.size(); ++i){
-        for(int j=0; j<triangle[i].size(); ++j){
-            max(answer+trianle[i][j],answer+trangle[i][j+1]);
-        }
-    }
-    
-    return answer;
+	int answer = 0;
+	int lastRow = triangle.size() - 1;
+	dp[0][0] = triangle[0][0];
+
+	for (int i = 0; i<triangle.size() - 1; ++i){
+		for (int j = 0; j<triangle[i].size(); ++j){
+			if (dp[i + 1][j] < dp[i][j] + triangle[i+1][j])
+				dp[i + 1][j] = dp[i][j] + triangle[i+1][j];
+			if (dp[i + 1][j + 1] = dp[i][j] + triangle[i+1][j+1])
+				dp[i + 1][j + 1] = dp[i][j] + triangle[i+1][j+1];
+		}
+	}
+	for (int i = 0; i<triangle[lastRow].size(); ++i){
+		answer = max(answer, dp[lastRow][i]);
+	}
+	return answer;
 }
